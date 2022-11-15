da3de3d9968ada915903fd110f89c8d2b02820d0
Anna Petras <apetras2017@gmail.com>
2022-11-15 15:47:14 +0100

    Refactor code to js to remove popup menu after click

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var script = document.createElement("script");
+script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
+document.getElementsByTagName("head")[0].appendChild(script);
+window.onload = function () {
+
 const divMenu = document.querySelector('.mobile-menu');
 const crossButton = document.querySelector('.cross-button');
 const hamburgerButton = document.querySelector('.hamb-bu');
@@ -13,3 +18,8 @@ crossButton.addEventListener('click', () => {
 hamburgerButton.addEventListener('click', () => {
   myFunction();
 });
+
+$(".mobile-menu li a").on("click", function () {
+    $("#cross-button").click();
+  });
+}
