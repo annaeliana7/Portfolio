e29ba9762307b5134f231a81cac373bb70ba190d
Anna Petras <apetras2017@gmail.com>
2022-11-15 10:21:25 +0100

    Refactored js code for mobile-menu popup

--- main.js
+++ main.js
@@ -1,28 +1,5 @@
-function myFunction() {
-  var x = document.getElementById("ham-bu");
-  if (x.style.display === "block") {
-   x.style.display = "none";
- } else {
-    x.style.display = "block";
- }
+const divMenu = document.querySelector('.mobile-menu') 
+
+const myFunction = () => {
+divMenu.classList.toggle('display')
 }
-
-// const navbarHamburger = document.querySelector('#hamb-bu');
-// const mobileMenu = document.querySelector('#mobile-menu');
-// const cross = document.querySelector('#cross-button');
-// navbarHamburger.addEventListener('click', () => {
-//   mobileMenu.style.display = 'block';
-// });
-
-// cross.addEventListener('click', () => {
-//   mobileMenu.style.display = 'none';
-// });
-
-// document.querySelectorAll('.nav-link').forEach(
-//   (link) => {
-//     link.addEventListener('click', () => {
-//       mobileMenu.style.display = 'none';
-//     });
-//   },
-// );
-
