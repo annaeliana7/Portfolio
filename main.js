3eddf3de2e8565ef493fe8db8324b6309eaa647a
Anna Petras <apetras2017@gmail.com>
2022-11-15 15:56:01 +0100

    Fix eslint errors

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-var script = document.createElement("script");
-script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
-document.getElementsByTagName("head")[0].appendChild(script);
+const script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+document.getElementsByTagName('head')[0].appendChild(script);
 window.onload = function () {
+  const divMenu = document.querySelector('.mobile-menu');
+  const crossButton = document.querySelector('.cross-button');
+  const hamburgerButton = document.querySelector('.hamb-bu');
 
-const divMenu = document.querySelector('.mobile-menu');
-const crossButton = document.querySelector('.cross-button');
-const hamburgerButton = document.querySelector('.hamb-bu');
+  const myFunction = () => {
+    divMenu.classList.toggle('display');
+  };
 
-const myFunction = () => {
-  divMenu.classList.toggle('display');
-};
-
-crossButton.addEventListener('click', () => {
-  myFunction();
-});
-
-hamburgerButton.addEventListener('click', () => {
-  myFunction();
-});
-
-$(".mobile-menu li a").on("click", function () {
-    $("#cross-button").click();
+  crossButton.addEventListener('click', () => {
+    myFunction();
   });
-}
+
+  hamburgerButton.addEventListener('click', () => {
+    myFunction();
+  });
+
+  $('.mobile-menu li a').on('click', () => {
+    $('#cross-button').click();
+  });
+};
