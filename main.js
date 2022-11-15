7a3318ef36d3755ce878b9fd87aad5e4b306b05c
Anna Petras <apetras2017@gmail.com>
2022-11-15 12:10:40 +0100

    Fix linter errors on main.js

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-const divMenu = document.querySelector('.mobile-menu') 
+const divMenu = document.querySelector('.mobile-menu');
+const crossButton = document.querySelector('.cross-button');
+const hamburgerButton = document.querySelector('.hamb-bu');
 
 const myFunction = () => {
-divMenu.classList.toggle('display')
-}
+  divMenu.classList.toggle('display');
+};
+
+crossButton.addEventListener('click', () => {
+  myFunction();
+});
+
+hamburgerButton.addEventListener('click', () => {
+  myFunction();
+});
